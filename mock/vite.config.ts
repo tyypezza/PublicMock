76c017d84c7ed7a52b9c6bea5d03a473e25fed63
tyypezza <123700440+tyypezza@users.noreply.github.com>
2023-10-18 23:03:37 -0400

    Update vite.config.ts

--- mock/vite.config.ts
+++ mock/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build',
+      outDir: "build",
     },
     plugins: [react()],
     server: {
       port: 8000,
     },
+    base: "/PublicMock/",
   };
 });
